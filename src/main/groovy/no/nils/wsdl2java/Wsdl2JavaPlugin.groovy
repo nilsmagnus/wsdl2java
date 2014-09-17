9468945a077d7dbda581034aa6e39b4b1d7be454
Nils Larsgård <nils.larsgard@norwegian.no>
2014-09-17 13:44:09 +0200

    Added a somehow working implementation of the plugin

--- src/main/groovy/no/nils/wsdl2java/Wsdl2JavaPlugin.groovy
+++ src/main/groovy/no/nils/wsdl2java/Wsdl2JavaPlugin.groovy
@@ -6,5 +6,11 @@ import org.gradle.api.Plugin
 class Wsdl2JavaPlugin implements Plugin<Project> {
     void apply(Project target) {
         target.task('wsdl2java', type: Wsdl2JavaTask)
+        target.afterEvaluate {
+        	target.configurations.create("wsdl2Java")
+        	target.dependencies.add("wsdl2Java", "org.apache.cxf:cxf-tools:$target.wsdl2java.cxfVersion")
+        }
     }
+
+   
 }
--- src/main/groovy/no/nils/wsdl2java/Wsdl2JavaTask.groovy
+++ src/main/groovy/no/nils/wsdl2java/Wsdl2JavaTask.groovy
@@ -1,13 +1,49 @@
 package org.gradle
 
+import org.apache.cxf.tools.wsdlto.WSDLToJava
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.TaskExecutionException
+
+import java.io.*
 
 class Wsdl2JavaTask extends DefaultTask {
-    String greeting = 'hello from wsdl2java'
-
+    String cxfVersion = "+"
+    String wsdlDir = "src/resources/wsdl" 
+    String generatedWsdlDir = "generated/src/main/java"
+	def wsdlsToGenerate
+    
     @TaskAction
     def wsdl2java() {
-        println greeting
-    }
+	    if (!new File(wsdlDir).exists()) {
+	    	def msg = "wsdlDir '$wsdlDir' does not exist"
+	    	throw new TaskExecutionException(this, new IllegalStateException(msg))
+	    } else {
+	    	wsdlsToGenerate.each{ args ->
+                println args
+	    		args.add(args.size -1 , '-d')
+	    		args.add(args.size -1 , generatedWsdlDir)
+                println args.size()
+                String[] stockArr = new String[args.size()];
+                for(int i=0;i<args.size(); i++)
+                    stockArr[i] = args[i]
+                WSDLToJava.main(stockArr)
+	    	}
+
+	       /* inputs.files wsdlDir.listFiles()
+	        outputs.files generatedWsdlDir
+	        doLast {
+	            wsdlsToGenerate.each { argsin ->
+	                argsin.add(argsin.size - 1, '-d')
+	                argsin.add(argsin.size - 1, generatedWsdlDir)
+	                javaexec {
+	                    classpath configurations.ws
+	                    main = 'org.apache.cxf.tools.wsdlto.WSDLToJava'
+	                    args = argsin
+	                    systemProperties = ['exitOnFinish':'TRUE']
+	                }
+	            }
+	        }*/
+	    }
+	}
 }
