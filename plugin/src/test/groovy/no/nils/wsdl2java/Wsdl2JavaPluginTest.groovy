8b6e7c177d95b41a5f66bdfba02bbab37cb3814b
Nils Larsgård <nils.larsgard@norwegian.no>
2014-09-26 07:24:12 +0200

    Added some tests

---
+++ plugin/src/test/groovy/no/nils/wsdl2java/Wsdl2JavaPluginTest.groovy
@@ -0,0 +1,25 @@
+package no.nils.wsdl2java
+
+import org.junit.Test
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+
+import static org.junit.Assert.assertTrue
+
+class Wsdl2JavaPluginTest{
+
+	@Test
+    public void canAddTaskToProject() {
+        Project project = ProjectBuilder.builder().build()
+        def task = project.task('wsdl2java', type: Wsdl2JavaTask)
+        assertTrue(task instanceof Wsdl2JavaTask)
+    }
+
+    @Test
+    public void greeterPluginAddsGreetingTaskToProject() {
+        Project project = ProjectBuilder.builder().build()
+        project.apply plugin: 'no.nils.wsdl2java'
+
+        assertTrue(project.tasks.wsdl2java instanceof Wsdl2JavaTask)
+    }
+}
