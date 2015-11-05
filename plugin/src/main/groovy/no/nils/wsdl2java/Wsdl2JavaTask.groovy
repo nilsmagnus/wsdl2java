97a27ee4d22e28fda94140a3574b67ef2b89e650
Jesper Skov <jskov@jyskebank.dk>
2015-11-05 14:20:07 +0100

    allow target encoding to be specified

--- plugin/src/main/groovy/no/nils/wsdl2java/Wsdl2JavaTask.groovy
+++ plugin/src/main/groovy/no/nils/wsdl2java/Wsdl2JavaTask.groovy
@@ -1,6 +1,7 @@
 package no.nils.wsdl2java
 
-import java.io.File;
+import java.io.File
+import java.security.MessageDigest;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -12,9 +13,12 @@ import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.TaskExecutionException
 
+import groovy.io.FileType;
+
 class Wsdl2JavaTask extends DefaultTask {
     // user properties
     String cxfVersion = "+"
+	String encoding = "UTF-8"
 
     @InputDirectory
     File wsdlDir = new File("src/main/resources")
@@ -31,6 +35,10 @@ class Wsdl2JavaTask extends DefaultTask {
     @TaskAction
     def wsdl2java() {
 		deleteOutputFolders()
+		MessageDigest md5 = MessageDigest.getInstance("MD5")
+		
+		File tmpDir = new File(project.getBuildDir(), "wsdl2java")
+		tmpDir.deleteDir()
 		
         if(classpath == null) {
             classpath = project.configurations.getByName(Wsdl2JavaPlugin.WSDL2JAVA)
@@ -38,8 +46,11 @@ class Wsdl2JavaTask extends DefaultTask {
         setupClassLoader()
         assert classLoader != null
         wsdlsToGenerate.each { args ->
+			String wsdlPath = md5.digest(args[-1].toString().bytes).encodeHex().toString()
+			File targetDir = new File(tmpDir, wsdlPath)
+			
             args.add(args.size - 1, '-d')
-            args.add(args.size - 1, generatedWsdlDir)
+            args.add(args.size - 1, targetDir)
             String[] wsdl2JavaArgs = new String[args.size()];
             for (int i = 0; i < args.size(); i++)
                 wsdl2JavaArgs[i] = args[i]
@@ -53,8 +64,30 @@ class Wsdl2JavaTask extends DefaultTask {
             } catch (Exception e) {
                 throw new TaskExecutionException(this, e)
             }
+			
+			copyToOutputDir(targetDir)
         }
     }
+	
+	protected void copyToOutputDir(File srcDir) {
+		int srcPathLength = srcDir.getAbsolutePath().size() + 1
+		
+		srcDir.eachFileRecurse(FileType.FILES) { file ->
+			String relPath = file.getAbsolutePath().substring(srcPathLength)
+			File target = new File(generatedWsdlDir, relPath)
+			
+			switchToEncoding(file)
+			project.ant.copy(file: file, tofile: target)
+		}
+	}
+
+	protected void switchToEncoding(File file) {
+		List<String> lines = file.getText().split(System.lineSeparator())
+		file.delete()
+	
+		String text = lines.join(System.lineSeparator()) + System.lineSeparator()  // want empty line last
+		file.withWriter(encoding) { w -> w.write(text) }
+	}
 
     private void setupClassLoader() {
         if (classpath?.files) {
@@ -90,5 +123,4 @@ class Wsdl2JavaTask extends DefaultTask {
 		}
 		return packagePaths;
 	}
-
 }
